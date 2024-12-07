069e9dbc9b1f5f92876a8ab890ecb4da98142539
Joab Ribeiro <joabrb@yahoo.com>
2024-12-07 09:49:45 -0300

    modificacao de codigo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/header";
+//import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import Login from './pages/login';
+import Menu from "./pages/menu/menu";
+
+function App() {
+	return (
+		<div className="App">
+			<Menu />
+			<Login />
+		</div>
+	);
+}
+
+export default App;
